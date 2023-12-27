cdd70ed7e9219832965c8604bf8f1fb0f2726ab5
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2023-12-27 22:11:21 +0200

    develop jwt auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,22 @@ import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { AuthController } from './auth.controller';
 
 @Module({
   providers: [AuthService, LocalStrategy],
-  imports: [UserModule, PassportModule],
+  controllers: [AuthController],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1200s' },
+    }),
+  ],
+  exports: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
