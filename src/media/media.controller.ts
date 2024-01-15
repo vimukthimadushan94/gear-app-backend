779f6db3787b3c0b03ccdd46313474e966caf45a
vimukthimadushan94 <madushan.gangoda94@gmail.com>
2024-01-15 21:48:57 +0200

    finalise post create

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,18 +1,14 @@
 import {
-  BadRequestException,
-  Body,
   Controller,
   Get,
+  Param,
   Post,
-  UploadedFile,
+  UploadedFiles,
   UseInterceptors,
-  UsePipes,
 } from '@nestjs/common';
 import { MediaService } from './media.service';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { FileUploadValidationPipe } from './file-upload-validation.pipe';
-import { FileUploadDto } from './dto/file-upload.dto';
 
 @Controller('api/media')
 export class MediaController {
@@ -23,26 +19,26 @@ export class MediaController {
     return this.mediaService.get();
   }
 
-  @Post('add')
+  @Post('upload-media/:postId')
   @UseInterceptors(
-    FileInterceptor('profilePicture', {
+    FilesInterceptor('images', 20, {
       storage: diskStorage({
-        destination: './uploads', // adjust the destination folder
+        destination: './uploads/posts',
         filename: (req, file, callback) => {
           const uniqueSuffix =
             Date.now() + '-' + Math.round(Math.random() * 1e9);
-          callback(null, file.fieldname + '-' + uniqueSuffix);
+          callback(null, `post-${uniqueSuffix}-${file.originalname}`);
         },
       }),
     }),
   )
-  @UsePipes(FileUploadValidationPipe)
-  async uploadFile(@UploadedFile() file, @Body() body: FileUploadDto) {
-    console.log('upoadingg..');
-    if (body.name) {
-      return 'File uploaded successfully!';
-    } else {
-      throw new BadRequestException('Name is required');
-    }
+  async uploadFile(
+    @UploadedFiles() images: Array<Express.Multer.File>,
+    @Param('postId') postId,
+  ) {
+    images.forEach((file) => {
+      this.mediaService.uploadFile(file, postId);
+    });
+    return images;
   }
 }
